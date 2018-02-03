455d36e66cfc87f07ad8128385ffe587e3f1ebcc
karliuka <karliuka.vitalii@gmail.com>
2018-02-03 14:49:01 +0200

    unistall

---
+++ Setup/Uninstall.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Copyright © 2011-2018 Karliuka Vitalii(karliuka.vitalii@gmail.com)
+ * 
+ * See COPYING.txt for license details.
+ */
+namespace Faonni\TrackingLink\Setup;
+
+use Magento\Framework\Setup\UninstallInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+use Magento\Config\Model\ResourceModel\Config\Data\CollectionFactory as ConfigCollectionFactory;
+
+/**
+ * TrackingLink Uninstall
+ */
+class Uninstall implements UninstallInterface
+{
+    /**
+     * Config Collection Factory
+     *
+     * @var \Magento\Config\Model\ResourceModel\Config\Data\CollectionFactory
+     */
+    private $_configCollectionFactory;
+
+    /**
+     * Initialize Setup
+     *
+     * @param ConfigCollectionFactory $configCollectionFactory
+     */
+    public function __construct(
+		ConfigCollectionFactory $configCollectionFactory
+	) {
+        $this->_configCollectionFactory = $configCollectionFactory;
+    }
+    
+    /**
+     * Uninstall DB Schema for a Module TrackingLink
+     *
+     * @param SchemaSetupInterface $setup
+     * @param ModuleContextInterface $context
+     * @return void
+     */
+    public function uninstall(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $setup->startSetup();
+        $this->removeConfig();	
+        $setup->endSetup();
+    }
+
+    /**
+     * Remove Config
+     *
+     * @return void
+     */
+    private function removeConfig()
+    {
+        $path = 'faonni_tracking/service_url';
+        /** @var \Magento\Config\Model\ResourceModel\Config\Data\Collection $collection */
+        $collection = $this->_configCollectionFactory->create(); 
+        $collection->addPathFilter($path);
+
+        foreach ($collection as $config) {
+			$config->delete(); 	
+        }
+    }    
+}
